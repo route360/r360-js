194c2acea655f2424b8e87c0bba162410258bddc
Daniel Gerber <daniel.gerber@deinestadtsuchtdich.de>
2014-06-07 18:34:49 +0200

    cloned leaflet repo structure created first structure for our lib

---
+++ Jakefile.js
@@ -0,0 +1,56 @@
+/*
+r360 building, testing and linting scripts.
+
+To use, install Node, then run the following commands in the project root:
+
+    npm install -g jake
+    npm install
+
+To check the code for errors and build r360 from source, run "jake".
+To run the tests, run "jake test".
+
+For a custom build, open build/build.html in the browser and follow the instructions.
+*/
+
+var build = require('./build/build.js'),
+    version = require('./src/r360.js').version;
+
+function hint(msg, paths) {
+	return function () {
+		console.log(msg);
+		jake.exec('node node_modules/jshint/bin/jshint -c ' + paths,
+				{printStdout: true}, function () {
+			console.log('\tCheck passed.\n');
+			complete();
+		});
+	}
+}
+
+desc('Check r360 source for errors with JSHint');
+task('lint', {async: true}, hint('Checking for JS errors...', 'build/hintrc.js src'));
+
+desc('Check r360 specs source for errors with JSHint');
+task('lintspec', {async: true}, hint('Checking for specs JS errors...', 'spec/spec.hintrc.js spec/suites'));
+
+desc('Combine and compress r360 source files');
+task('build', {async: true}, function (compsBase32, buildName) {
+	var v;
+
+	jake.exec('git log -1 --pretty=format:"%h"', {}, function () {
+		build.build(complete, v, compsBase32, buildName);
+
+	}).on('stdout', function (data) {
+		v = version + ' (' + data.toString() + ')';
+	})
+});
+
+desc('Run PhantomJS tests');
+task('test', ['lint', 'lintspec'], {async: true}, function () {
+	build.test(complete);
+});
+
+task('default', ['test', 'build']);
+
+jake.addListener('complete', function () {
+  process.exit();
+});
---
+++ src/r360.js
@@ -0,0 +1,28 @@
+
+var L = {
+	version: '0.1-dev'
+};
+
+function expose() {
+	var oldr360 = window.r360;
+
+	L.noConflict = function () {
+		window.r360 = oldr360;
+		return this;
+	};
+
+	window.r360 = r360;
+}
+
+// define r360 for Node module pattern loaders, including Browserify
+if (typeof module === 'object' && typeof module.exports === 'object') {
+	module.exports = r360;
+
+// define r360 as an AMD module
+} else if (typeof define === 'function' && define.amd) {
+	define(r360);
+
+// define r360 as a global r360 variable, saving the original r360 to restore later if needed
+} else {
+	expose();
+}
