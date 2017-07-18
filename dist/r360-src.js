fd1a6f850d71f77616f31c3e1a96e44b4eeafc21
Adam Roberts <roberts@motionintelligence.net>
2017-07-18 17:01:04 +0200

    hotfix build

--- dist/r360-src.js
+++ dist/r360-src.js
@@ -1,5 +1,5 @@
 /*
- Route360° JavaScript API v0.4.2 ("df06f97"), a JS library for leaflet maps. http://route360.net
+ Route360° JavaScript API v0.4.2 (e94217c), a JS library for leaflet maps. http://route360.net
  (c) 2017 Henning Hollburg, Daniel Gerber and Jan Silbersiepe, (c) 2017 Motion Intelligence GmbH
 */
 (function (window, document, undefined) {
