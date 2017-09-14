4f76a7dfa4a7b3c5080277562f60a1654aee009d
Adam Roberts <roberts@motionintelligence.net>
2017-09-14 15:17:41 +0200

    repo cleanup

--- lib-esm/index.js
+++
@@ -1,7 +0,0 @@
-import { defaults } from './modules/config';
-import * as basemapUtils from './modules/basemaps';
-export var version = 'v1.0.0';
-export var config = defaults;
-export var getbasemapList = basemapUtils.getBasemapList;
-export var getGLStyle = basemapUtils.getGLStyle;
-//# sourceMappingURL=index.js.map
